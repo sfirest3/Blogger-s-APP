55f94176f404f25c8e82718a69cae6c6826f017d
sfirest3 <sfirest3@lion.lmu.edu>
2024-10-31 13:20:40 -0700

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAy4xQp_KcashjmllKSvfHFgzbuG0gS4ds",
-    authDomain: "bloogg-a5946.firebaseapp.com",
-    projectId: "bloogg-a5946",
-    storageBucket: "bloogg-a5946.firebasestorage.app",
-    messagingSenderId: "64395961318",
-    appId: "1:64395961318:web:4e12fe2dc58ff08184ec27"
+    
 }
 
 export const app = initializeApp(firebaseConfig)
